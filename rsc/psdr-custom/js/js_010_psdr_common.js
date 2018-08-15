9a951ae4637c12c05fd03adb5f093bd96cedb669
sunabove <sunabove@nate.com>
2018-08-16 07:29:18 +0900

    **

--- rsc/psdr-custom/js/js_010_psdr_common.js
+++ rsc/psdr-custom/js/js_010_psdr_common.js
@@ -1556,4 +1556,13 @@ $("#logoutButton").click(
 
 /** -- common listeners **/
 
+/** validation */
+
+function isValidEmail( email ) {
+	var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	return regex.test(email);
+}
+
+/** -- validation */
+
 // -- ars common java script file
